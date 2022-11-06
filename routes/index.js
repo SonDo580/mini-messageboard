6c1387bf53af3bf1ab816aadabfb4a654ed8b276
Son Do <dominhson580@gmail.com>
2022-11-06 23:54:59 +0700

    Pass different template variables

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ const messages = [
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini MessageBoard", messages: messages });
 });
 
 module.exports = router;
