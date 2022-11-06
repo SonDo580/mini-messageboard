ecfcb7f3df2546aadd944aefdc844cf6251e78b9
Son Do <dominhson580@gmail.com>
2022-11-06 23:49:29 +0700

    Add sample messages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
