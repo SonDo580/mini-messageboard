85d14e7cf46105022d5c4163ab1cb352ad4e78f8
Son Do <dominhson580@gmail.com>
2022-11-07 11:17:37 +0700

    Change the path

--- routes/new.js
+++ routes/new.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/new", function (req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("form");
 });
