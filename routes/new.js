2e49a0b09e556f8ccdd61e720673dde4a7ea63a8
Son Do <dominhson580@gmail.com>
2022-11-07 15:53:09 +0700

    Delete new.js

--- routes/new.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", function (req, res, next) {
-  res.render("form", { title: "Message Form" });
-});
-
-module.exports = router;
