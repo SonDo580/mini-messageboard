1f390857135ade0b208e418f25083b4f07ab18dc
Son Do <dominhson580@gmail.com>
2022-11-07 11:23:24 +0700

    Pass 'title' variable to 'form' template

--- routes/new.js
+++ routes/new.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.render("form");
+  res.render("form", { title: "Message Form" });
 });
 
 module.exports = router;
